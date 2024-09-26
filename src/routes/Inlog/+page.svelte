757bfe14ae38ca70e4fdee9fa1c4ceda552ec815
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-26 13:16:07 +0200

    CW -  login content adjustment
    - test text
    - aria-label
    - image change

--- src/routes/Inlog/+page.svelte
+++ src/routes/Inlog/+page.svelte
@@ -12,12 +12,19 @@
 		
 		<div class="login-content">
 			<h1>Mobility package</h1>
-			<img src="P5211348.jpg" alt="background image for the login page" >
+
+			<div class="login-info-highlight">
+				<h1>Deloitte</h1>
+				<p>Deloitte is a global professional services firm that provides a wide range of services to clients in the private and public sectors. Our services include consulting, project management, risk management, and more. We work with clients to help them achieve their business goals and objectives.</p>
+				<p>Deloitte is a global professional services firm that provides a wide range of services to clients in the private and public sectors. Our services include consulting, project management, risk management, and more. We work with clients to help them achieve their business goals and objectives.</p>
+				<p>Deloitte is a global professional services firm that provides a wide range of services to clients in the private and public sectors. Our services include consulting, project management, risk management, and more. We work with clients to help them achieve their business goals and objectives.</p>
+			</div>
+			<img src="pexels-dan-voican-2624103-17854203.jpg" alt="background image for the login page" >
 		</div>
 		
 		
 		<article >
-			<h1>Log-in</h1>
+			<h1 aria-label="login">Log-in</h1>
 			<p>some extra text</p>
 			<div class="space"></div>
 
