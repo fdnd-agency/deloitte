7787ff0c65d6ccaab6bcf70f91304ccb15cc64c9
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-27 14:20:47 +0200

    cw - general setup

--- src/hooks.server.js
+++
@@ -1,7 +0,0 @@
-export async function handle({ event, resolve }) {
-	return await resolve(event, {
-		filterSerializedResponseHeaders: (key, value) => {
-			return key.toLowerCase() === 'content-type';
-		},
-	});
-}
--- src/routes/+page.js
+++
@@ -1,9 +0,0 @@
-import getDirectusInstance from "$lib/directus";
-import { readItem } from "@directus/sdk";
-export async function load({fetch}){
-	const directus = getDirectusInstance(fetch);
-	return {
-		//this needs to be changed 
-		// global: await directus.request(readItems('global')),
-	}
-}
