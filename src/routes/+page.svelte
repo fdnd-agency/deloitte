fc9942aeec297c7216fe2f2a27bc7f702e81b0e9
Mossati <xmossati9786@gmail.com>
2024-09-27 16:23:09 +0200

    global CSS update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     export let data;
+
+    import '../styles/global.css'
 </script>
 
 <h1>{data.person.name}</h1>
+
+<button class="button-1">Test</button>
+<button class="button-2">Test</button>
+<button class="button-3">Test</button>
+
+<input type="checkbox" name="test" value="test">
