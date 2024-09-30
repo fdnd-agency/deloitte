3d607e0138afe157d0ef2a6d79bfb9e34bf28226
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-01 00:23:00 +0200

    CW - profile page script

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	// export let data;
+	// import '../../styles/profile.css'
+	import '../../styles/global.css'
+	// import { error } from './+error.svelte';
+	// import { page } from '$app/stores';
+
+	let score = 0;
+
+	$: score = 2;
+	
+
+</script>
