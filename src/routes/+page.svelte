fe9ccd0639d025a69a6207f870d374a1a1cba81e
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-01 14:47:21 +0200

    CW - general page changes
    - refactoring
    - added goto module
    - added redirect (does not work )

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
     export let data;
+    
+	export async function load({ url }) {
+    return {
+      status: 302,
+      redirect: '/Inlog'
+    };
+  }
+
 </script>
 
 <h1>{data.person.name}</h1>
+
+
+
+<style>
+
+
+  
+</style>
