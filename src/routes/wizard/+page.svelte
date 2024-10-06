56ad5ba3ff087c1787ba436e9cfe6a5514320c22
Mossati <xmossati9786@gmail.com>
2024-10-06 09:03:50 +0200

    [Mohammed] Directus fetch

---
+++ src/routes/wizard/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let data;
+</script>
+  
+  {#if data.questions && data.questions.length > 0} 
+    <ul>
+      {#each data.questions as question}
+        <li>{question.question} {question.id}</li> 
+      {/each}
+    </ul>
+  {:else}
+    <p>Er zijn geen vragen beschikbaar.</p>
+  {/if}
+  
