72226d26b4d577cf3f3edcf6366766547b2a831f
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-09 14:48:43 +0200

    CW - button component
    added
    -  component classes
    -  restprops
    -  variables
    - button states

--- src/lib/button-component.svelte
+++ src/lib/button-component.svelte
@@ -1,16 +1,30 @@
 <script>
 	export let text;
-	export let data;	
-	export let color
+	// export let data;	
+	export let color;
+	export let colorLine = color;
+	export let task;
+
+	let buttonProps = {
+    class:[$$restProps.class]
+    }
 </script>
 
-<button on:click={data} style="border: solid {color};">
+{#if task.length > 1}
+
+<a href="{task}" style="border: solid {colorLine}; color:{color};" {...buttonProps}>
+	{text}
+</a>
+{:else}
+
+<button on:click={()=>{task}} style="border: solid {colorLine}; color:{color}; --button-color:{color};" {...buttonProps}>
 	{text}
 </button>
+{/if}
 
 <style>
 
-	button{
+	button,a{
 		--D-white2:rgba(255, 255, 255, 0.514);
 		width: fit-content;
 		height: fit-content;
@@ -21,19 +35,19 @@
 		background-image: linear-gradient(1deg,var(--D-mid-bk) 5%, rgb(46, 200, 202) 46%);
 		box-shadow: inset 0 -1px 5px var(--D-white2);		
 		border: none;
-		font-size: clamp(1rem, 0.0217rem + 4.3478vw, 3.5rem);
+		/* font-size: clamp(1rem, 0.0217rem + 4.3478vw, 3.5rem); */
 		contain:content;
 		container-type:normal;
 		container-name: button;
 		text-wrap:nowrap ;
 	}
 
-	button:hover{
+	:is(button,a):hover{
 		box-shadow: inset 0 -1px 9px var(--D-white2);		
 
 	}
 
-	button:active{
+	:is(button,a):active{
 		box-shadow: inset 0 -1px 5px var(--D-dark-support);	
 		box-shadow: inset 0 -1px 5px var(--D-white);	
 		background-image: linear-gradient(5deg,var(--D-mid-bk) 5%, rgb(46, 200, 202) 46%);
@@ -42,10 +56,44 @@
 		transition-property: box-shadow, scale;
 	}
 
-	button:focus-visible{
+	:is(button,a):focus-visible{
 		outline: solid ;
 		background-image: none;
 		background-color: var(--D-dark-support);
+	}
+
+	.nextPage{
+		width: 100%;
+		height: 7cqh;
+		font-size: clamp(1rem, 0.0217rem + 4.3478vw, 4cqh);
+		margin-bottom: 10%;
+		color: white;
+		background-color: var(--black);
+		border-radius:.8pc;
+		border: none;
+		display: grid;
+		place-content: center;
+
+		&:focus-visible,:focus-within{
+			background-color: var(--D-mid-bk);
+			outline-style: dashed;
+			color: var(--D-black);
+		}
+	}
+
+	.logout{
+		width: 100%;
+		background-image: none;
+		background-color: var(--D-white);
+		border-radius: 5px;
+
+		
+	}
+
+	.logout:active{
+		/* background-image: inherit; */
+		background-color:var(--button-color) ;
+
 	}
 
 </style>		
