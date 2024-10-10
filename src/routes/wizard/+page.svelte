085eb2b0856f7760069285954ac898e18d600223
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-10-10 15:06:58 +0200

    CW - wizard redesign

--- src/routes/wizard/+page.svelte
+++ src/routes/wizard/+page.svelte
@@ -1,12 +1,14 @@
 <script>
   import {Checkbox} from '$lib';
+	import ButtonComponent from '$lib/button-component.svelte';
+	import ComponentBox from '$lib/ComponentBox.svelte';
   import {totalScore} from '$lib/stores'
   
   export let data; // Export de data
 
   let currentQuestionIndex = 0; // Huidige vraagnummer
   let selectedAnswer = null; // Geselecteerde antwoord
-  // let $totalScore = 0; // Totale score
+   $totalScore = 0; // Totale score
   let userAnswers = []; // Gebruiker antwoorden array
   let selectedPackage = null; // Geselecteerde pakket
 
@@ -54,59 +56,166 @@
 </script>
 
 <!-- check of er al een pakket is gekozen -->
+
+<ComponentBox
+    heading="Mobility program"
+		context1=""
+		context2=""
+		display="flex"
+		titleAlign="center"
+		class="top-c"
+
+/>
+
+
+
 {#if selectedPackage === null}
-  <form on:submit={nextQuestion}>
-    <!-- Toont huidige vraagnummer -->
-    <p>Vraag {currentQuestionIndex + 1}/{data.questions.length}</p>
 
-    <!-- Toont de huidige score -->
-    <p>Score: {$totalScore}</p>
+<form on:submit={nextQuestion}>
+   <!-- wizard module -->
+   <!-- Toont de huidige vraag -->
+   <!-- Toont huidige vraagnummer -->
+   <!-- Toont de huidige score -->
+  
+  <ComponentBox
+    
+        heading="Vraag {currentQuestionIndex + 1}/{data.questions.length}"
+        context1="Score: {$totalScore}"
+        context2=""
+        display="flex"
+        titleAlign=""
+        class="box1 wizard"
+    >
+        <a href="/overview">Lees meer over de diverse mobilitiet opties</a>
 
-    <!-- Toont de huidige vraag -->
-    <h2>{data.questions[currentQuestionIndex].question}</h2>
-
-    <!-- Loopt door de antwoorden en toont alleen de antwoorden die bij de huidige vraag horen -->
-    <ul>
-      <!-- fetch de antwoorden en filter ze op de question_id en vergelijk die met de huidige vraag id -->
-      {#each data.answers.filter(answer => answer.question_id === data.questions[currentQuestionIndex].id) as answer}
-        <li>
-          <!-- bind de waarde van de gekozen antwoord aan het component en het antwoord zelf  -->
-          <Checkbox {answer} bind:bindGroup={selectedAnswer}/>
-        </li>
-      {/each}
-    </ul>
-
+      <ComponentBox
+        heading="{data.questions[currentQuestionIndex].question}"
+        context1=""
+        context2=""
+        display="flex"
+        titleAlign=""
+        class="checkboxBox"
+      >
+      <!-- Loopt door de antwoorden en toont alleen de antwoorden die bij de huidige vraag horen -->
+      <ul>
+        <!-- fetch de antwoorden en filter ze op de question_id en vergelijk die met de huidige vraag id -->
+        {#each data.answers.filter(answer => answer.question_id === data.questions[currentQuestionIndex].id) as answer}
+          <li>
+            <!-- bind de waarde van de gekozen antwoord aan het component en het antwoord zelf  -->
+            <Checkbox {answer} bind:bindGroup={selectedAnswer}/>
+          </li>
+        {/each}
+      </ul>
+      </ComponentBox>
     <!-- Knop om naar de volgende vraag te gaan -->
-    <button type="submit">Volgende vraag</button>
-  </form>
+     <div class="buttonBox">
+
+       <ButtonComponent
+           text="Vorige vraag"
+           color="black"
+           colorLine="none"
+           task=""
+           class=""
+           type=""/>
+   
+       <ButtonComponent
+           text="Volgende vraag"
+           color="black"
+           colorLine="none"
+           task=""
+           class=""
+           type="submit"/>
+          </div>
+        </ComponentBox>
+        </form>
 {:else}
-  <!-- Toont het geselecteerde pakket na het beantwoorden van alle vragen -->
+<!-- Toont het geselecteerde pakket na het beantwoorden van alle vragen -->
+<ComponentBox
+    heading="Jouw aanbevolen pakket: {selectedPackage.package_name}"
+		context1="{selectedPackage.description}"
+		context2="Score: {$totalScore}"
+		display="flex"
+		titleAlign=""
+		class="box1"
+
+>
+
   <div>
-    <h2>Jouw aanbevolen pakket: {selectedPackage.package_name}</h2>
-    <p>{selectedPackage.description}</p>
-    <p>Score: {$totalScore}</p>
     <p>Antwoorden:</p>
     <pre>{JSON.stringify(userAnswers, null, 2)}</pre> <!-- Toont de antwoorden in JSON formaat -->
   </div>
+</ComponentBox>
+
+
+
 {/if}
+
 
 <style>
   form {
     display: flex;
     flex-direction: column;
+
+    & h1{
+      color: var(--D-dark-support);
+      margin-bottom: 1%;
+    }
+
+    & p{
+     font-size: 2rem;
+     margin-bottom: 1%;
+    }
+
+    &  a{
+      margin-bottom: 3%;
+    }
   }
+
 
   ul {
     display: flex;
     flex-direction: column;
-    gap: 0.5rem;
+    gap: 1.1rem;
+
+   
+
   }
 
-  button {
-    margin-top: 16px;
+  li{
+    height: 7vh;
+    width: 70%;
+    max-width: 30vw;
+    font-size: clamp(10px, 50vw - 2rem, 1.5rem);
+
+    
+    & label{
+     /* outline: solid red; */
+      width: 100%;
+      height: 100%;
+      border-radius: 3.5pc;
+    }
   }
+
+
+
+  .buttonBox{
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+    gap: 15%;
+    width: 35vw;
+    height: 3rem;
+    margin-top: 20px;
+
+    & button{
+      width: 100%;
+      height: 100%;
+      padding-inline: 5%;
+    }
+  }
+
 
   div {
-    margin-top: 20px;
+  
   }
 </style>
