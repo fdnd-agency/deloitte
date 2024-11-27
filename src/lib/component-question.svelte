5db17ffdab49f500093ac50f8893edf4a70734dc
Mossati <xmossati9786@gmail.com>
2024-11-27 14:56:22 +0100

    question component

---
+++ src/lib/component-question.svelte
@@ -0,0 +1,30 @@
+<h2>Vraag</h2>
+<p>Kies 1 van de onderste opties</p>
+<ul>
+    <li>...</li>
+</ul>
+
+<style>
+    h2 {
+        font-size: 1.5rem;
+        color: #212121;
+    }
+
+    p {
+        color: #212121;
+        font-weight: 300;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    @media only screen and (min-width: 48rem) {
+        ul {
+            flex-direction: row;
+        }
+    }
+</style>
