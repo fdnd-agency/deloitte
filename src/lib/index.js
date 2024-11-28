c062416af79c77ae855ee5cee8429a0069f99e76
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-11-28 15:11:27 +0100

    components organization

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,11 +3,12 @@
 export { default as ComponentBox } from './systems/ComponentBox.svelte';
 export { default as WinC } from './systems/Window.svelte'
 
-export { default as Button } from './atom/button-component.svelte';
+// export { default as Button } from './atom/old-button-component.svelte';
 export { default as Checkbox } from './atom/checkbox.svelte';
-export { default as Footer } from './mulecule/footer.svelte';
-export { default as Header } from './mulecule/header.svelte';
 export { default as MobilityOptions } from './atom/mobility-options.svelte';
 export { default as GeneralInfo} from './atom/general-info.svelte'
-export {default as PackageComponent} from './mulecule/package-component.svelte'
 export { default as Button } from './atom/Button.svelte'
+
+export { default as Footer } from './mulecule/footer.svelte';
+export { default as Header } from './mulecule/header.svelte';
+export {default as PackageComponent} from './mulecule/package-component.svelte'
