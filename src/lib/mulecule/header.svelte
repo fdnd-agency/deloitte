bd6979b137efe3130ccde219db810855fc1174a2
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-11-28 15:09:43 +0100

    header update
    added
    - logo and styling
    - shadow

--- src/lib/mulecule/header.svelte
+++ src/lib/mulecule/header.svelte
@@ -39,15 +39,50 @@
 
 <style>
     header {
+        position: relative;
         width: 100%;
         padding-inline: var(--B-padding-inline);
+        padding-block: 2%;
         position: sticky;
+        box-shadow: 0 10px 5px -10px rgba(0, 0, 0, 0.278);
     }
 
     nav {
+        position: relative;
+        align-items: center;
+        flex-wrap: nowrap;
+    }
+
+    a {
         display: flex;
         flex-direction: row;
-        justify-content: space-between;
         align-items: center;
+        text-decoration: none;
+        color: var(--D-t-support,var(--LD-text));
+        min-width: fit-content;
+        width: fit-content;
+    }
+
+    a h1 {
+        /* width: 100%; */
+        width: fit-content;
+        font-size: 1.5rem;
+        font-weight: 700;
+        padding-inline: 1cqw;
+        margin: 0;
+        color: var(--LD-text);
+    }
+
+    a h1 em {
+        color: var(--text-tertiary,var(--LD-text));
+        border-left: solid 1px ;
+        padding-left: 5px;
+        text-align: start;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+
+    a:hover em{
+        translate:0px -10px;
     }
 </style>
