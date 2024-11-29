1291845691255fd45c0bfde9285d1f3f934e6aa2
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-11-29 14:09:43 +0100

    component index update

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,12 @@
-export { default as ComponentBox } from './ComponentBox.svelte';
-
-// Export other components as needed, for example:
-export { default as Button } from './button-component.svelte';
-export { default as Footer } from './footer.svelte';
-export { default as Header } from './header.svelte';
-export { default as Package } from './package-component.svelte';
-export { default as MobilityOptions } from './mobility-options.svelte';
-export { default as GeneralInfo} from './general-info.svelte'
-export {default as PackageComponent} from './package-component.svelte'
+//atoms
+export { default as Button } from './atom/Button.svelte'
+export { default as MobilityOptions } from './atom/mobility-options.svelte';
+export { default as GeneralInfo} from './atom/general-info.svelte'
+// Mulecule
+export { default as Footer } from './mulecule/footer.svelte';
+export { default as Header } from './mulecule/header.svelte';
+export {default as PackageComponent} from './mulecule/package-component.svelte'
+// system
+export { default as ComponentBox } from './systems/ComponentBox.svelte';
+export { default as WinC } from './systems/Window.svelte'
+export { default as Overview } from './systems/overview/overview.svelte';
