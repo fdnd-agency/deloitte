432754200930443a474081c8251d1a35dcde22f7
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-12-30 10:48:05 +0100

    view-transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,21 @@
     import {Header,Footer} from '$lib';
     import '../styles/global.css'
     import { WinC,Button } from '$lib';
+    import {onNavigate} from '$app/navigation'
 
     /** @type {{children?: import('svelte').Snippet}} */
     let { children } = $props();
+
+    onNavigate((navigation) => {
+        if(!document.startViewTransition){return};
+
+        return new Promise((resolve) =>{
+            document.startViewTransition(async ()=>{
+                resolve();
+                await navigation.complete;
+            })
+        })
+    })
 
     // import Footer from '$lib/footer.svelte';
 </script>
