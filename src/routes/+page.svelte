75536c85a5f94f823c763ece873f9b0477baa686
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-01-20 16:46:52 +0100

    updated homepage functionality and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   	// ==================================================
     import { WinC,Button,Overview } from '$lib';
 	import { onMount } from 'svelte';
-	import Section from '$lib/atom/component-section.svelte';
+	import Section from '$lib/atom/Section.svelte';
 	
 	// ==================================================
   	// Props
@@ -13,6 +13,7 @@
 	let { data } = $props();
 	let loggedIn = $state(false);
 	let loggedInUser = data.users.find(user => user.id === data.userID) || null;
+	let newProfile = $state(false);
 	
 	// ==================================================
   	// Login handler
@@ -39,36 +40,46 @@
 		}
 
 		event.preventDefault();
+
+		$inspect(loggedInUser, data.userID, loggedIn)
+		
 	}
 </script>
 
-{#if !loggedIn}
-<Section
-subtitle="Enter your personal information"
+{#if !loggedIn }
+<WinC
+role="child"
 title="Login"
-body="Enter your credentials">
-	<form onsubmit={handleLogin}>
-	<label>
+context=""
+color='lightblue'
+class="main-panel not-logged"
+>
+<form onsubmit={handleLogin}>
+		<p>Enter your personal information</p>
+	<!-- <label>
 		E-mail
-		<input type="email" name="email" placeholder="enter your email">
-	</label>
-	<label>
+	</label> -->
+	<input type="email" name="email" placeholder="enter your email" required>
+	<!-- <label>
 		Password
-		<input type="password" name="password" placeholder="enter your password">
-	</label>
-	<button type="submit">Login</button>
+	</label> -->
+	<input type="password" name="password" placeholder="enter your password" required>
+	<Button 
+	sort="submit" 
+	text='Login' 
+	color='inherit'
+	class='login'/>
 	</form>
-</Section>
+</WinC>
 
-{:else if userlogedIn}
+{:else if loggedIn && data.userID >= 1}
 	<WinC
 	role="child"
-	title="Deloitte"
+	title={loggedInUser?.name}
 	context='Strart nu de vragenlijst om een passend pakket te vinden'
 	color='lightblue'
 	class="main-panel"
 	>
-
 	<p>Cookie: {data.userID}</p>
 	<p>{loggedInUser?.name}</p>
 	<p>{loggedInUser?.email}</p>
@@ -78,8 +89,7 @@ body="Enter your credentials">
 		color=''
 		class='buttonB'>
 
-
-			{#if newProfile}
+			{#if data.userID < 1}
 				<Button
 				sort="/wizard"
 				text="Start Nu"
@@ -87,13 +97,11 @@ body="Enter your credentials">
 				/>
 			{:else}
 
-
 				<Button
 				sort="/profile"
 				text="profile"
 				color="white"
 				/>
-
 
 				<Button
 				sort="/wizard"
@@ -103,7 +111,7 @@ body="Enter your credentials">
 			{/if}		
 		</WinC>
 	</WinC>
-{:else}
+<!-- {:else}
 	<WinC
 	role="child"
 	title="Deloitte"
@@ -115,9 +123,8 @@ body="Enter your credentials">
 		text="Start hier"
 		color="black"
 		/>
-	</WinC>
+	</WinC> -->
 {/if}
-
 
 <WinC
 role="child"
@@ -129,6 +136,17 @@ class="info-panel"
 	<p>info about what why the options exist and how to use them</p>
 </WinC>
 
+<WinC
+role="child"
+title="Pakketten"
+context='Deloitte biedt een maandelijkse mobiliteitsvergoeding van € 200. Hiermee kun je een abonnement nemen op een deelfiets of een ov-abonnement. Daarnaast kun je ook een elektrische fiets aanschaffen met een eenmalige bijdrage van € 1000.'
+color=""
+class="package-panel"
+>
+	<!-- cards list with pakketten -->
+	<p>info about what why the options exist and how to use them</p>
+	<Overview data/>
+</WinC>
 
 <WinC
 role="child"
@@ -141,22 +159,6 @@ class="info-panel"
 </WinC>
 
 
-<WinC
-role="child"
-title="Pakketten"
-context='Deloitte biedt een maandelijkse mobiliteitsvergoeding van € 200. Hiermee kun je een abonnement nemen op een deelfiets of een ov-abonnement. Daarnaast kun je ook een elektrische fiets aanschaffen met een eenmalige bijdrage van € 1000.'
-color=""
-class="package-panel"
->
-	<!-- cards list with pakketten -->
-	<p>info about what why the options exist and how to use them</p>
-	<Overview data/>
-
-
-
-
-</WinC>
-
 
 <WinC
 role="child"
@@ -168,7 +170,6 @@ class="info-panel"
 	<p>info about what why the options exist and how to use them</p>
 </WinC>
 
-
 <WinC
 role="child"
 title="Mobiliteitregeling"
@@ -178,7 +179,6 @@ class="info-panel"
 >
 	<p>info about what why the options exist and how to use them</p>
 </WinC>
-
 
 <style>
 	:global(main .main-panel){
@@ -190,8 +190,10 @@ class="info-panel"
 
 		& > h2{
 			font-size: clamp(2rem, 0.917rem + 4.1478vw, 6cqi);
+			height: fit-content;
+			z-index: 1;
 		}
-
+	
 		&::before{
 			content: '';
 			position: absolute;
@@ -200,19 +202,42 @@ class="info-panel"
 			left: -20cqw;
 			width: clamp(50cqw, 100vw + 10vw, 70cqw);
 			max-width: 100vw;
-			/* aspect-ratio: 1; */
 			background-color: var(--D-t-support);
 			opacity: 0.2;
 			z-index: 0;
 			border-radius: 50%;
-
-
+			transition: 1s ease-out;
 		}
 
 		@container  (width <= 550px){
+
+			flex-direction: column !important;
+
 			&::before{
-				width: clamp(200cqw, 100vw - 15rem, 300cqw);
+				width: clamp(200cqw, 100vw - 15rem, 300cqw) !important;
 			}
+		}
+
+		&:global(.not-logged)  {
+			display: flex;
+			flex-direction: row ;
+			flex-wrap: wrap ;
+			gap: 5%;
+
+			@container  (width <= 850px){
+	
+	
+				&::before{
+					width: clamp(90cqw, 100vw - 11rem, 200cqw);
+				}
+			}
+		}
+
+		&:global(.not-logged) form {
+			display: flex;
+			flex-direction: column;
+			width: 22cqw ;
+			align-self: stretch;
 		}
 
 		&> .buttonB{
@@ -220,7 +245,6 @@ class="info-panel"
 			--cc-radius:1pc;
 			--w:40;
 			--p: calc(calc(100 - var(--w))/2);
-
 			background: none ;
 			gap: 5%;
 			padding-inline: 0;
@@ -239,16 +263,63 @@ class="info-panel"
 			}
 		}
 
+		form {
+			z-index: 3;
+			display: flex;
+			gap: 1rem;
+			width: fit-content;
+			padding: 1%;
+			margin-block: 1%;
+			border-radius: var(--cc-radius,inherit);
+		}
+
+		form label{
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+
+		form input{
+			padding: 3%;
+			border-radius:  var(--cc-radius,inherit);
+			border: none;
+			font-size: clamp(1rem,1.3cqw, 2rem);
+			padding-inline: 5%;
+
+
+		}
+
+		form input:focus-within:invalid{
+			background-color: color-mix(in srgb, currentcolor 50%, rgba(251, 139, 139, 0.845) );
+			outline: none;
+		}
+
+		form input:focus-within:valid{
+			background-color: white;
+			outline: solid 1px color-mix(in srgb, var(--D-t-support) 50%, rgb(187, 192, 28) );
+		}
+
+		form :global(.login){
+			width: 100%;
+			height: fit-content;
+			font-size: 1rem;
+			place-self: center;
+			justify-self: center;
+			padding: 3%;
+			padding-inline: 5%;
+			margin-block: 0.5cqh;
+			font-size: clamp(1rem,5cqw, 2rem);
+			text-transform: capitalize;
+		}
+
 	}
-
-	
-
 
 	:global(.info-panel){
 		width: 100%;
 		height: fit-content;
 		border-radius: 0px !important;
 		padding-block: 6cqh !important;
+
 		&> h2{
 			color: var(--D-dark-support,var(--LD-text));
 			font-size: 1.3rem;
@@ -275,7 +346,6 @@ class="info-panel"
 			color: var(--D-dark-support,var(--LD-text));
 			font-size: 2rem;
 		}
-		
 	}
 
 	p{	
@@ -286,7 +356,5 @@ class="info-panel"
 	:global(.box1,.box2){
 		border: solid 1px !important;
 	}
-	
-	
 	
 </style>
